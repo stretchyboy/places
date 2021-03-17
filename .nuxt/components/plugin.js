9bb222a7f857aa3c75c100fb1856b9c90a602ef8
Martyn Eggleton <martyn.eggleton@gmail.com>
2021-03-17 20:49:39 +0000

    initial nuxt bild
    Signed-off-by: Martyn Eggleton <martyn.eggleton@gmail.com>

---
+++ .nuxt/components/plugin.js
@@ -0,0 +1,11 @@
+import Vue from 'vue'
+
+const components = {
+  Card: () => import('../../components/Card.vue' /* webpackChunkName: "components/card" */).then(c => c.default || c),
+  Logo: () => import('../../components/Logo.vue' /* webpackChunkName: "components/logo" */).then(c => c.default || c)
+}
+
+for (const name in components) {
+  Vue.component(name, components[name])
+  Vue.component('Lazy' + name, components[name])
+}
